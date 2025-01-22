beff55e3a3a9b59a537890ff1af7c7b1eff68e38
vdlrwnsv12 <vdlrwnsv12@gmail.com>
2025-01-22 21:07:34 +0900

    카드 난이도 별 간격

--- Assets/scripts/Board.cs
+++ Assets/scripts/Board.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.Collections.LowLevel.Unsafe;
 using UnityEngine;
 using System.Linq;
 
@@ -11,25 +10,54 @@ public class Board : MonoBehaviour
     public int idx = 0;
     public int cardCount = 0;
 
+    public enum CardNumber
+    {
+        easy = 6,
+        normal = 8,
+        Hard = 10
+    }
 
+    [SerializeField]
+    public CardNumber cardNum = CardNumber.normal;
 
     void Start()
     {
-        int[] arr = { 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7 };
-        arr = arr.OrderBy(x => Random.Range(0f, 7f)).ToArray();
+        int cardTypeCount = (int)cardNum;
 
-        for (int i = 0; i < 16; i++)
+        int[] arr = new int[cardTypeCount * 2]; //CardNumber 곱하기 2 ex)Hard면 20
+        for (int i = 0; i < cardTypeCount; i++)
+        {
+            arr[i] = i;
+            arr[cardTypeCount + i] = i;
+        }
+
+        arr = arr.OrderBy(x => Random.Range(0f, arr.Length)).ToArray();
+
+        for (int i = 0; i < arr.Length; i++)
         {
             GameObject go = Instantiate(card);
             go.transform.SetParent(board, false);
 
             float x = (i % 4) * 1.4f - 2.1f;
-            float y = (i / 4) * 1.6f - 3.0f;
-            go.transform.position = new Vector2(x, y);
+            if (cardNum == CardNumber.Hard) //난이도 별 카드 간격 조정 
+            {
+                float y = (i / 4) * 1.5f - 4.0f;
+                go.transform.position = new Vector2(x, y);
+
+            } else if(cardNum == CardNumber.easy)
+            {
+                float y = (i / 4) * 1.6f - 2.5f;
+                go.transform.position = new Vector2(x, y);
+            } else
+            {
+                float y = (i / 4) * 1.6f - 3.0f;
+                go.transform.position = new Vector2(x, y);
+            }
+            
 
             go.GetComponent<Card>().Setting(arr[i]);
         }
+
         GameManager.Instance.cardCount = arr.Length;
     }
-
 }
